7e4b8b47bad4f9dbec72c4e30b1684c173f25bfd
naveenrn55 <60521910+naveenrn55@users.noreply.github.com>
2020-02-13 15:04:34 +0530

    Update Zone.java

--- src/main/java/com/mindtree/propertytax/entity/Zone.java
+++ src/main/java/com/mindtree/propertytax/entity/Zone.java
@@ -14,6 +14,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -27,6 +28,7 @@ import org.hibernate.annotations.GenericGenerator;
  * </pre>
  */
 @Entity
+@Table(name = "zone")
 public class Zone {
     /**
      * int id.
