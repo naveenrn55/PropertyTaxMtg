194b24f1d10ad4a9ba12f3749722a4e06120e273
naveenrn55 <60521910+naveenrn55@users.noreply.github.com>
2020-02-13 15:04:59 +0530

    Update CollectedTax.java

--- src/main/java/com/mindtree/propertytax/entity/CollectedTax.java
+++ src/main/java/com/mindtree/propertytax/entity/CollectedTax.java
@@ -3,6 +3,7 @@ package com.mindtree.propertytax.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * <pre>
