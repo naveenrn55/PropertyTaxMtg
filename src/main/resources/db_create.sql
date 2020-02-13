867a703aec6af85eb265f05aa8ff29634aa133ac
naveenrn55 <60521910+naveenrn55@users.noreply.github.com>
2020-02-13 15:10:46 +0530

    Update db_create.sql

--- src/main/resources/db_create.sql
+++ src/main/resources/db_create.sql
@@ -1,12 +1,12 @@
-insert into Zone values (1,"North");
+insert into zone values (1,"North");
 
-insert into PropertyType values(1,"land");
-insert into PropertyType values(2,"car");
+insert into propertytype values(1,"land");
+insert into propertytype values(2,"car");
 
-insert into ResidentialStatus values(1,"own");
-insert into ResidentialStatus values(2,"rented");
+insert into residentialstatus values(1,"own");
+insert into residentialstatus values(2,"rented");
 
-insert into TaxCost values("land","own","North",20);
-insert into TaxCost values("car","rented","North",20);
-insert into TaxCost values("land","rented","North",20);
-insert into TaxCost values("car","own","North",20);
+insert into taxcost values("land","own","North",20);
+insert into taxcost values("car","rented","North",20);
+insert into taxcost values("land","rented","North",20);
+insert into taxcost values("car","own","North",20);
